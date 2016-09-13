12efa9dae1c15184860a7445f87e2062539819bc
Phil Werli <werli@fim.uni-passau.de>
2016-09-13 13:36:46 +0200

    Added gateways to the storage index wrapper file. added more functionality to service objects

--- serios/core/storage/index.js
+++ serios/core/storage/index.js
@@ -5,18 +5,41 @@
 
 var storage = null;
 
+/**
+ * These are all the methods the storage type has to support.
+ */
 module.exports = {
     init: init,
 
-    // below are the methods the storage type has to support
+    // service objects
     addServiceObject: function (so) {
         storage.addServiceObject(so);
     },
-    editServiceObject: function (so) {
-        storage.editServiceObject(so);
+    updateServiceObject: function (soID, so) {
+        storage.updateServiceObject(soID, so);
     },
     removeServiceObject: function (soID) {
         storage.removeServiceObject(soID);
+    },
+    getAllSoForUser: function (soID) {
+        storage.getAllSoForUser(soID);
+    },
+    getAllSoForGateway: function (gatewayID) {
+        storage.getAllSoForGateway(gatewayID);
+    },
+
+    // gateways
+    addGateway: function (gateway) {
+        storage.addGateway(gateway);
+    },
+    updateGateway: function (gatewayID, gateway) {
+        storage.updateGateway(gatewayID, gateway);
+    },
+    removeGateway: function (gatewayID) {
+        storage.removeGateway(gatewayID);
+    },
+    getAllGatewaysForUser: function (userID) {
+        storage.getAllGatewaysForUser(userID);
     }
 };
 
