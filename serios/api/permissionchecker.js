329dd05814a28a4511acce89f7aa96974bb8a297
Phil Werli <werli@fim.uni-passau.de>
2016-11-18 14:30:37 +0100

    updated dummy permission checker

--- serios/api/permissionchecker.js
+++ serios/api/permissionchecker.js
@@ -4,11 +4,15 @@
  It serves as wrapper of the identity management which uses callbacks.
  */
 module.exports = {
-    checkPermission: function () {
+    checkPermission: function (authorization_token) {
         return new Promise(function (resolve, reject) {
             // this is a dummy version for later authentication
             // We will use identity management from Juan David
-            return resolve();
+            if (authorization_token) {
+                resolve(authorization_token);
+            } else {
+                reject();
+            }
         });
     }
 };
