4b18310616038eaeb9072805e52837e57800e2ce
dschreckling <ds@sec.uni-passau.de>
2017-01-11 19:45:10 +0100

    Adapt settings file to oauth2 integration

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     storage: {
         type: "mongodb",
-        location: "mongodb://localhost/serios-database",
+        location: "mongodb://localhost:27017/serios-database",
         max_number_of_sensor_data_saved : 10000,
         max_duration_of_sensor_data_saved: {
             value: 30,
@@ -14,135 +14,247 @@ module.exports = {
             timeunit: "days"
         }
     },
+    oauth2: {
+        authorizationURL: 'http://localhost:3000/idm/oauth2/dialog/authorize',
+        tokenURL: 'http://localhost:3000/idm/oauth2/token',
+        clientID: "SERIOS_Dashboard",
+        clientSecret: "somesecret",
+        callbackURL: "http://localhost:3000/idm/auth/callback",
+        userInfoUrl: 'http://localhost:3000/idm/oauth2/api/userinfo',
+    },
+    ui : {
+        "site": {
+            "tls": {
+                "key": "./certs/server.key",
+                "cert": "./certs/server.crt"
+            },
+            "https_port": 1445,
+            "http_port": 3002
+        },
+        "idm": {
+            "host": "localhost",
+            "port": 3000,
+            "protocol": "http"
+        }
+    },
     idm : {
         core : {
             "storage": {
-                "dbName": "./database"
+                "dbName": "db/database_"
             },
-            "authentication": {
-                "web-server": "http://127.0.0.1:3000/api/",
-                
-            },
-            "schema-validation": [
-                {
-                    "id": "/Sensor",
-                    "type": "object",
-                    "properties": {
-                        "name": {
-                            "type": "string"
-                        },
-                        "token": {
-                            "type": "string"
+            "policies": {
+                "dbName": "./policies.json",
+                "create_entity_policy": [
+                    // actions of an actor are not restricted a priori
+                    {
+                        target: {
+                            type: "any"
+                        }
+                    }, {
+                        source: {
+                            type: "any"
+                        }
+                    }
+                ],
+                "top_level_policy": [
+                    // all properties can be read by everyone
+                    {
+                        target: {
+                            type: "any"
                         }
                     },
-                    "required": ["name"]
-                },
-                {
-                    "id": "/SimpleAddress",
-                    "type": "object",
-                    "properties": {
-                        "lines": {
-                            "type": "array",
-                            "items": {
-                                "type": "string"
+                    // all properties can only be changed by the owner of the entity
+                    {
+                        source: {
+                            type: "user"
+                        },
+                        locks: [{
+                            lock: "isOwner"
+                        }]
+                    }, {
+                        source: {
+                            type: "user"
+                        },
+                        locks: [{
+                            lock: "attrEq",
+                            args: ["role", "admin"]
+                        }]
+                    }
+                ],
+                "attribute_level_policies": {
+                    "user": {
+                        "password": [
+                            // the property can only be read by the user itself
+                            {
+                                target: {
+                                    type: "user"
+                                },
+                                locks: [{
+                                    lock: "isOwner"
+                                }]
+                            },
+                            // the property can be set by the user itself and
+                            {
+                                source: {
+                                    type: "user"
+                                },
+                                locks: [{
+                                    lock: "isOwner"
+                                }]
+                            },
+                            // by all users with role admin
+                            {
+                                source: {
+                                    type: "user"
+                                },
+                                locks: [{
+                                    lock: "attrEq",
+                                    args: ["role", "admin"]
+                                }]
                             }
-                        },
-                        "zip": {
-                            "type": "string"
-                        },
-                        "city": {
-                            "type": "string"
-                        },
-                        "country": {
-                            "type": "string"
-                        }
+                        ],
+                        "role": [
+                            // can be read by everyone
+                            {
+                                target: {
+                                    type: "any"
+                                }
+                            },
+                            // can only be changed by users with role admin
+                            {
+                                source: {
+                                    type: "user"
+                                },
+                                locks: [{
+                                    lock: "attrEq",
+                                    args: ["role", "admin"]
+                                }]
+                            }
+                        ]
                     },
-                    "required": ["country", "lines"]
-                },
-                {
-                    "id": "/SimplePerson",
-                    "type": "object",
-                    "properties": {
-                        "name": {
-                            "type": "string"
-                        },
-                        "address": {
-                            "$ref": "/SimpleAddress"
-                        },
-                        "votes": {
-                            "type": "integer",
-                            "minimum": 1
-                        }
-                    },
-                    "required": ["address"]
-                    
-                }]
-        },
-        webui : {
-            "gateway_id":"1",
-            "token-storage":{
-                "dbName":"./tokens.sqlite3",
-                "createTables":true
+                    "sensor": {
+                        "credentials": [
+                            // the property can only be read by the user itself
+                            {
+                                target: {
+                                    type: "user"
+                                },
+                                locks: [{
+                                    lock: "isOwner"
+                                }]
+                            },
+                            // the property can be set by the user itself and
+                            {
+                                source: {
+                                    type: "user"
+                                },
+                                locks: [{
+                                    lock: "isOwner"
+                                }]
+                            }
+                        ]
+                    }
+
+                }
             },
-            "auth":{
-                "response":{
-                    "success":{
-                        "url":"/static/index.html",
-                        "token":"query-params"
+            "schema-validation": [{
+                "id": "/sensor",
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string"
                     },
-                    "fail":{
-                        "url":"/static/error/error.html"
+                    "credentials": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "system": {
+                                    "type": "string"
+                                },
+                                "value": {
+                                    "type": "string"
+                                }
+                            }
+                        }
                     }
                 },
-                "github":{
-                    "clientID":"dummyID",
-                    "clientSecret":"dummySecret",
-                    "redirect_path":"http://localhost:3000/auth/callback_github",
-                    "scope":[
+                "required": ["name"]
+            }, {
+                "id": "/user",
+                "type": "object",
+                "properties": {
+                    "user_name": {
+                        "type": "string"
+                    },
+                    "auth_type": {
+                        "type": "string"
+                    },
+                    "password": {
+                        "type": "string"
+                    },
+                    "role":{
+                        "type":"string"
+                    }
+                },
+                "required": ["user_name", "auth_type"]
+            }, {
+                "id": "/client",
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string"
+                    },
+                    "clientSecret": {
+                        "type": "string"
+                    },
+                    "redirectURI": {
+                        "type": "string"
+                    }
+                },
+                "required": ["name", "clientSecret", "redirectURI"]
+            }]
+        },
+        ui : {
+            "gateway_id": "1",
+            "token-storage": {
+                "dbName": "./database_web",
+                "createTables": true
+            },
+            "failureRedirect" : "/idm/login",
+            "auth": {
+                "github": {
+                    "clientID": "getGithubId",
+                    "clientSecret": "getGithubSecret",
+                    "redirect_path": "http://localhost:3000/auth/callback_github",
+                    "scope": [
                         "notifications"
                     ]
                 },
-                "dropbox":{
-                    "clientID":"getDropboxId",
-                    "clientSecret":"getDropboxSecret",
-                    "redirect_path":"http://localhost:3000/auth/callback_dropbox",
-                    "scope":[
+                "dropbox": {
+                    "clientID": "getDropboxId",
+                    "clientSecret": "getDropboxSecret",
+                    "redirect_path": "http://localhost:3000/auth/callback_dropbox",
+                    "scope": [
                         ""
                     ]
                 },
-                "google":{
-                    "clientID":"getGoogleId",
-                    "clientSecret":"getGoogleSecret",
-                    "redirect_path":"http://localhost:3000/auth/callback_google",
-                    "scope":[
+                "google": {
+                    "clientID": "getGoogleId",
+                    "clientSecret": "getGoogleSecret",
+                    "redirect_path": "http://localhost:3000/auth/callback_google",
+                    "scope": [
                         "https://www.googleapis.com/auth/drive",
                         "https://www.googleapis.com/auth/userinfo.profile",
                         "https://www.googleapis.com/auth/userinfo.email"
                     ]
-                },
-                "local":{
-                    "response":{
-                        "success":{
-                            "url":"/static/idm/authenticateUser.html"
-                        }
-                    },
-                    "fallback-user-no-pam":{
-                        "username":"admin",
-                        "password":"correcthorsebatterystaple"
-                    }
-                },
-                "web-id":{
-
                 }
             },
-            "tls":{
-                "key":"./certs/server.key",
-                "cert":"./certs/server.crt"
+            "tls": {
+                "key": "./certs/server.key",
+                "cert": "./certs/server.crt"
             },
-            "http_port" : 3000,
-	        "https_port" : 1443,
-	        "enabledStrategies" : ["agile-local","github","google","webid"]
-
+            "enabledStrategies": ["agile-local", "github", "google", "webid"]
         }
     }
 };
