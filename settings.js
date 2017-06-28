7d74f21073b1135c3584a3e3818d0d6db4c61b7a
dschreckling <dschrecking@users.noreply.github.com>
2017-06-28 11:37:14 +0200

    Simplify settings after deleting components

--- settings.js
+++ settings.js
@@ -1,9 +1,12 @@
 module.exports = {
-    server : {
-        tls : false,
-        host : "127.0.0.1",
-        port : 3000,
-        cluster : 0
+    server: {
+        tls: false,
+        host: "127.0.0.1",
+        port: 3000,
+        cluster: 0
+    },
+    rest: {
+        prefix: "serios"
     },
     storage: {
         type: "mongodb",
@@ -22,282 +25,50 @@ module.exports = {
         testuser: "seriosTest",
         testpassword: "seriosTest"
     },
-    oauth2: {
-        authorizationURL: 'http://localhost:3000/idm/oauth2/dialog/authorize',
-        tokenURL: 'http://localhost:3000/idm/oauth2/token',
-        clientID: "SERIOS_Dashboard",
-        clientSecret: "somesecret",
-        callbackURL: "http://localhost:3000/idm/auth/callback",
-        userInfoUrl: 'http://localhost:3000/idm/oauth2/api/userinfo',
-    },
-    ui : {
-        "site": {
-            "tls": {
-                "key": "./certs/server.key",
-                "cert": "./certs/server.crt"
-            },
-            "https_port": 1445,
-            "http_port": 3002
-        },
-        "idm": {
-            "host": "localhost",
-            "port": 3000,
-            "protocol": "http"
-        }
-    },
-    idm : {
-        core : {
-            "storage": {
-                "dbName": "db/database_"
-            },
-            "policies": {
-                "dbName": "./policies.json",
-                "create_entity_policy": [
-                    // actions of an actor are not restricted a priori
-                    {
-                        target: {
-                            type: "any"
-                        }
-                    }, {
-                        source: {
-                            type: "any"
-                        }
-                    }
-                ],
-                "top_level_policy": [
-                    // all properties can be read by everyone
-                    {
-                        target: {
-                            type: "any"
-                        }
+    security: {
+            pdp : {
+                ulocks: {
+                    entityTypes : {
+                        "/any"    :  0,
+                        "/group"  :  1,
+                        "/user"   :  2,
+                        "/sensor" :  3,
+                        "/client" :  4,
+                        "/api"    :  5,
+                        "/const"  :  6,
+                        "/attr"   :  6,
+                        "/prop"   :  6,
+                        "/var"    :  6,
                     },
-                    // all properties can only be changed by the owner of the entity
-                    {
-                        source: {
-                            type: "user"
-                        },
-                        locks: [{
-                            lock: "isOwner"
-                        }]
-                    }, {
-                        source: {
-                            type: "user"
-                        },
-                        locks: [{
-                            lock: "attrEq",
-                            args: ["role", "admin"]
-                        }]
-                    }
-                ],
-                "attribute_level_policies": {
-                    "user": {
-                        "password": [
-                            // the property can only be read by the user itself
-                            {
-                                target: {
-                                    type: "user"
-                                },
-                                locks: [{
-                                    lock: "isOwner"
-                                }]
-                            },
-                            // the property can be set by the user itself and
-                            {
-                                source: {
-                                    type: "user"
-                                },
-                                locks: [{
-                                    lock: "isOwner"
-                                }]
-                            },
-                            // by all users with role admin
-                            {
-                                source: {
-                                    type: "user"
-                                },
-                                locks: [{
-                                    lock: "attrEq",
-                                    args: ["role", "admin"]
-                                }]
-                            }
-                        ],
-                        "role": [
-                            // can be read by everyone
-                            {
-                                target: {
-                                    type: "any"
-                                }
-                            },
-                            // can only be changed by users with role admin
-                            {
-                                source: {
-                                    type: "user"
-                                },
-                                locks: [{
-                                    lock: "attrEq",
-                                    args: ["role", "admin"]
-                                }]
-                            }
-                        ]
-                    },
-                    "sensor": {
-                        "credentials": [
-                            // the property can only be read by the user itself
-                            {
-                                target: {
-                                    type: "user"
-                                },
-                                locks: [{
-                                    lock: "isOwner"
-                                }]
-                            },
-                            // the property can be set by the user itself and
-                            {
-                                source: {
-                                    type: "user"
-                                },
-                                locks: [{
-                                    lock: "isOwner"
-                                }]
-                            }
-                        ]
-                    }
-
+                    locks: "./security/Locks/",
+                    actions: "./security/Actions"
                 }
             },
-            "schema-validation": [{
-                "id": "/sensor",
-                "type": "object",
-                "properties": {
-                    "name": {
-                        "type": "string"
-                    },
-                    "credentials": {
-                        "type": "array",
-                        "items": {
-                            "type": "object",
-                            "properties": {
-                                "system": {
-                                    "type": "string"
-                                },
-                                "value": {
-                                    "type": "string"
-                                }
-                            }
-                        }
-                    }
-                },
-                "required": ["name"]
-            }, {
-                "id": "/user",
-                "type": "object",
-                "properties": {
-                    "user_name": {
-                        "type": "string"
-                    },
-                    "auth_type": {
-                        "type": "string"
-                    },
-                    "password": {
-                        "type": "string"
-                    },
-                    "role":{
-                        "type":"string"
-                    }
-                },
-                "required": ["user_name", "auth_type"]
-            }, {
-                "id": "/client",
-                "type": "object",
-                "properties": {
-                    "name": {
-                        "type": "string"
-                    },
-                    "clientSecret": {
-                        "type": "string"
-                    },
-                    "redirectURI": {
-                        "type": "string"
-                    }
-                },
-                "required": ["name", "clientSecret", "redirectURI"]
-            }]
-        },
-        ui : {
-            "gateway_id": "1",
-            "token-storage": {
-                "dbName": "./database_web",
-                "createTables": true
+        pap: {
+            server: {
+                "host": "localhost",
+                port: 1234,
+                path: "/pap/",
+                tls: false,
+                cluster: 1
             },
-            "failureRedirect" : "/idm/login",
-            "auth": {
-                "github": {
-                    "clientID": "getGithubId",
-                    "clientSecret": "getGithubSecret",
-                    "redirect_path": "http://localhost:3000/auth/callback_github",
-                    "scope": [
-                        "notifications"
-                    ]
-                },
-                "dropbox": {
-                    "clientID": "getDropboxId",
-                    "clientSecret": "getDropboxSecret",
-                    "redirect_path": "http://localhost:3000/auth/callback_dropbox",
-                    "scope": [
-                        ""
-                    ]
-                },
-                "google": {
-                    "clientID": "getGoogleId",
-                    "clientSecret": "getGoogleSecret",
-                    "redirect_path": "http://localhost:3000/auth/callback_google",
-                    "scope": [
-                        "https://www.googleapis.com/auth/drive",
-                        "https://www.googleapis.com/auth/userinfo.profile",
-                        "https://www.googleapis.com/auth/userinfo.email"
-                    ]
+            storage: {
+                type: "mongodb",
+                host: "localhost",
+                port: 27017,
+                password: "",
+                user: "",
+                dbName: "pap-database",
+                collection: "policies",
+                
+                cache: {
+                    enabled: false,
+                    TTL: 600,
+                    sync: {
+                        type: "redis",
+                        channel: "policyUpdates"
+                    }
                 }
-            },
-            "tls": {
-                "key": "./certs/server.key",
-                "cert": "./certs/server.crt"
-            },
-            "enabledStrategies": ["agile-local", "github", "google", "webid"]
-        }
-    },
-    neros: {
-        uiPort : 3000,
-        mqttReconnectTime: 15000,
-
-        // Retry time in milliseconds for Serial port connections
-        serialReconnectTime: 15000,
-
-        // The maximum length, in characters, of any message sent to the debug sidebar tab
-        debugMaxLength: 1000,
-
-        functionGlobalContext: {
-            // os:require('os'),
-            // octalbonescript:require('octalbonescript'),
-            // jfive:require("johnny-five"),
-            // j5board:require("johnny-five").Board({repl:false})
-        },
-
-        // Configure the logging output
-        logging: {
-            // Only console logging is currently supported
-            console: {
-                // Level of logging to be recorded. Options are:
-                // fatal - only those errors which make the application unusable should be recorded
-                // error - record errors which are deemed fatal for a particular request + fatal errors
-                // warn - record problems which are non fatal + errors + fatal errors
-                // info - record information about the general running of the application + warn + error + fatal errors
-                // debug - record information which is more verbose than info + info + warn + error + fatal errors
-                // trace - record very detailed logging + debug + info + warn + error + fatal errors
-                level: "info",
-                // Whether or not to include metric events in the log output
-                metrics: false,
-                // Whether or not to include audit events in the log output
-                audit: false
             }
         }
     }
